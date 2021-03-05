7fb054cf0cd885fae13c6853f064709b5689d7d5
yango3 <o3.yang@hotmail.com>
2021-03-05 15:44:07 +0800

    Update

--- solidity/PIn.sol
+++ solidity/PIn.sol
@@ -2,17 +2,85 @@ pragma solidity ^0.4.25;
 
 import "./POSBase.sol";
 import "./Table.sol";
+import "./POSLib.sol";
 
-contract PIn is POSBase{
+contract PIn is POSBase {
+    using POSLib for POSLib.ErrCode;
+
     string constant TABLE_NAME = "pos_in";
 
-    constructor(string loc) POSBase(loc){
+    constructor(string loc) POSBase(loc) {
         createTable(loc);
     }
-    function createTable(string loc) private{
+
+    function createTable(string loc) private {
         getTableFactory().createTable(
-            TABLE_NAME, 
-            loc, 
-            "berth_id,index,in_time,in_time_type,in_type,plate_id,prepay_len,prepay_money,vehicle_type,in_pic_hash");
+            TABLE_NAME,
+            loc,
+            "berth_id,index,in_time,in_time_type,in_type,plate_id,prepay_len,prepay_money,vehicle_type,in_pic_hash"
+        );
+    }
+
+    function _insert(
+        string berthId,
+        string inTime,
+        int256 inTimeType,
+        int256 inType,
+        string plateId,
+        int256 prepayLen,
+        int256 prepayMoney,
+        int256 vehicleType,
+        string inPicHash
+    ) private returns (POSLib.ErrCode) {
+        Table table = openTable(TABLE_NAME);
+        Entry entry = table.newEntry();
+        entry.set("berth_id", berthId);
+        entry.set("index", index);
+        incIndex();
+        entry.set("in_time", inTime);
+        entry.set("in_time_type", inTimeType);
+        entry.set("in_type", inType);
+        entry.set("plate_id", plateId);
+        entry.set("prepay_len", prepayLen);
+        entry.set("prepay_money", prepayMoney);
+        entry.set("vehicle_type", vehicleType);
+        entry.set("in_pic_hash", inPicHash);
+
+        if (table.insert(LOC, entry) == 1) {
+            return POSLib.ErrCode.OK;
+        } else {
+            return POSLib.ErrCode.FAIL;
+        }
+    }
+
+    function insertRecord(
+        string berthId,
+        string inTime,
+        int256 inTimeType,
+        int256 inType,
+        string plateId,
+        int256 prepayLen,
+        int256 prepayMoney,
+        int256 vehicleType,
+        string inPicHash
+    ) public returns (POSLib.ErrCode) {
+        Entries entries = getByStr(TABLE_NAME, "berth_id", berthId);
+
+        if (entries.size() != 0) {
+            return POSLib.ErrCode.EXISTS;
+        } else {
+            return
+                _insert(
+                    berthId,
+                    inTime,
+                    inTimeType,
+                    inType,
+                    plateId,
+                    prepayLen,
+                    prepayMoney,
+                    vehicleType,
+                    inPicHash
+                );
+        }
     }
 }
--- solidity/POSBase.sol
+++ solidity/POSBase.sol
@@ -10,7 +10,7 @@ contract POSBase {
     address private owner;
 
     // name 
-    string private LOC;
+    string internal LOC;
 
     constructor(string loc)public{
         owner = msg.sender;
@@ -32,12 +32,31 @@ contract POSBase {
     function getLoc() public view returns(string){
         return LOC;
     }
+    function incIndex() internal{
+        index++;
+    }
 
     function getTableFactory() internal returns(TableFactory){
         return TableFactory(0x1001);
     }
     function openTable(string tableName) internal returns (Table) {
         return getTableFactory().openTable(tableName);
+    }
+    function getByStr(string tableName,string column, string val) internal returns (Entries){
+        Table table = openTable(tableName);
+
+        Condition condition = table.newCondition();
+        condition.EQ(column, val);
+        return table.select(LOC, condition);
+
+    }
+    function getByNum(string tableName,string column, int256 num) internal returns (Entries){
+        Table table = openTable(tableName);
+
+        Condition condition = table.newCondition();
+        condition.EQ(column, num);
+        return table.select(LOC, condition);
+
     }
 
 }
